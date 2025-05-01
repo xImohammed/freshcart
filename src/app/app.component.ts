b9d5e1d1fc00a75fc0d867c4ec2b36da5f3c2677
xImohammed <mohammeddarafaa.1@gmail.com>
2025-05-02 00:29:58 +0300

    CHANGED

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,10 @@ import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from "./feature/layout/components/footer/footer.component";
 import { NgxSpinnerComponent } from 'ngx-spinner';
 import { CommonModule } from '@angular/common';
-import { ParticlesBackgroundComponent } from "./shared/components/particles-background/particles-background.component";
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FooterComponent, NgxSpinnerComponent, CommonModule, ParticlesBackgroundComponent],
+  imports: [RouterOutlet, FooterComponent, NgxSpinnerComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { AuthComponent } from './core/auth/auth/auth.component';
 import { MainlayoutComponent } from './feature/layout/mainlayout/mainlayout.component';
 import { AuthGuard } from './core/guards/auth.guard';
 import { loggedGuard } from './core/guards/logged.guard';
+
 export const routes: Routes = [
   {
     path: '',
@@ -11,7 +12,8 @@ export const routes: Routes = [
   },
   {
     path: '',
-    component: AuthComponent,canActivate:[loggedGuard],
+    component: AuthComponent,
+    canActivate: [loggedGuard],
     children: [
       {
         path: 'login',
@@ -22,13 +24,15 @@ export const routes: Routes = [
         loadComponent: () => import('./core/auth/components/register/register.component').then(m => m.RegisterComponent)
       },
       {
-        path:'forget',loadComponent:()=> import('./core/auth/components/forgetpass/forgetpass.component').then(c =>c.ForgetpassComponent)
+        path: 'forget',
+        loadComponent: () => import('./core/auth/components/forgetpass/forgetpass.component').then(c => c.ForgetpassComponent)
       }
     ]
   },
   {
     path: '',
-    component: MainlayoutComponent,canActivate:[AuthGuard],
+    component: MainlayoutComponent,
+    canActivate: [AuthGuard],
     children: [
       {
         path: 'home',
@@ -51,18 +55,26 @@ export const routes: Routes = [
         loadComponent: () => import('./feature/pages/products/products.component').then(m => m.ProductsComponent)
       },
       {
-        path:'product-details/:id',loadComponent:()=>import('./feature/pages/product-details/product-details.component').then(c=>c.ProductDetailsComponent)
+        path: 'product-details/:id',
+        loadComponent: () =>
+          import('./feature/pages/product-details/product-details.component').then(
+            (c) => c.ProductDetailsComponent
+          ),
+        data: { renderMode: 'client' } 
+      }
+      ,
+      {
+        path: 'check-out',
+        loadComponent: () => import('./feature/pages/check-out/check-out.component').then((c) => c.CheckOutComponent)
       },
       {
-        path:'check-out',loadComponent:()=>import('./feature/pages/check-out/check-out.component').then((c)=>c.CheckOutComponent)
-      },
-      {
-        path:'allorders',loadComponent:()=>import('./feature/pages/allorders/allorders.component').then((c)=>c.AllordersComponent)
+        path: 'allorders',
+        loadComponent: () => import('./feature/pages/allorders/allorders.component').then((c) => c.AllordersComponent)
       }
     ]
   },
   {
     path: '**',
-    loadComponent:()=>import('./feature/pages/notfound/notfound.component').then(c => c.NotfoundComponent)
+    loadComponent: () => import('./feature/pages/notfound/notfound.component').then(c => c.NotfoundComponent)
   }
 ];
