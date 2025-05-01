36c9fe5d5d0417f4f68cad0d75e4e13faf16c265
xImohammed <mohammeddarafaa.1@gmail.com>
2025-05-02 01:47:20 +0300

    latestt version

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -10,6 +10,7 @@ import { base_Url } from './core/custom_injection/api_BaseUrl';
 import { headersInterceptor } from './core/Interceptors/headers/headers.interceptor';
 import { errorInterceptor } from './core/Interceptors/error/error.interceptor';
 import { loadingInterceptor } from './core/Interceptors/loading/loading.interceptor';
+import { redirect_Url } from './core/custom_injection/redirect_Url';
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()),
     provideHttpClient(withFetch(),withInterceptors([headersInterceptor,errorInterceptor,loadingInterceptor])),
@@ -17,6 +18,10 @@ export const appConfig: ApplicationConfig = {
     {
       provide: base_Url,
       useValue:'https://ecommerce.routemisr.com/api/v1/'
+    },
+    {
+      provide:redirect_Url,
+      useValue:'freshcart-git-main-ximohammeds-projects.vercel.app'
     },
     provideToastr()
     ,importProvidersFrom(NgxSpinnerModule)
---
+++ src/app/core/custom_injection/redirect_Url.ts
@@ -0,0 +1,3 @@
+import { InjectionToken } from "@angular/core";
+
+export const redirect_Url = new  InjectionToken<string>('redirectUrl')
